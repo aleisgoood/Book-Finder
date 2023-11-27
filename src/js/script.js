0911ec768892a2b64a1495b11eb5e420f558f791
Ale <vale.alessia@yahoo.it>
2023-11-27 13:49:19 +0100

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -5,23 +5,13 @@ function searchBooks() {
   resultsContainer.innerHTML = '';
   descriptionContainer.innerHTML = '';
 
-  
-  
-
-
   function searchBooks() {
-    const searchInput = document.getElementById('searchInput').value.toLowerCase();
-    const resultsContainer = document.getElementById('results');
-    const descriptionContainer = document.getElementById('descriptionContainer');
-    resultsContainer.innerHTML = '';
-    descriptionContainer.innerHTML = '';
-
     if (!validGenres.includes(searchInput)) {
       resultsContainer.innerHTML = '<p>Error: Please enter a valid genre.</p>';
       return;
     }
   }
-  
+//subjects/genres searching 
   fetch(`https://openlibrary.org/subjects/${searchInput}.json`)
     .then(response => {
       if (!response.ok) {
@@ -33,11 +23,11 @@ function searchBooks() {
       if (!data.works || data.works.length === 0) {
         throw new Error('No books found for the given category. Please check your spelling and try again.');
       }
-
+//books divs
       data.works.forEach(work => {
         const bookElement = document.createElement('div');
         bookElement.classList.add('book');
-        bookElement.textContent = `Title: ${work.title}, Authors: ${work.authors.map(author => `<strong>${author.name}</strong>`).join(', ')}`;
+        bookElement.textContent = `Title: ${work.title}, Authors: ${work.authors.map(author => `${author.name}`).join(', ')}`;
         bookElement.addEventListener('click', () => getBookDescription(work.key));
         resultsContainer.appendChild(bookElement);
       });
@@ -48,13 +38,14 @@ function searchBooks() {
     });
 }
 
+//get the description in the container
 function getBookDescription(bookKey) {
   const descriptionContainer = document.getElementById('descriptionContainer');
 
   fetch(`https://openlibrary.org${bookKey}.json`)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Book not found. Please check your spelling.');
+        throw new Error('Book not found. Please check your spelling and try again.');
       }
       return response.json();
     })
