5770549dba748f9ad12a5b745f78439ce1c06ee9
Ale <vale.alessia@yahoo.it>
2023-11-28 16:10:03 +0100

    fixed styling - background, h1, reset buttons

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,18 @@ function searchBooks() {
   const resultsContainer = document.getElementById('results');
   const descriptionContainer = document.getElementById('descriptionContainer');
   const h1Elem = document.querySelector('h1');
-  
-  //make h1 disappear after loading the results
-  h1Elem.style.display = 'none';
+  const searchInputElem = document.getElementById('searchInput');
+  const searchButtonElem = document.getElementById('searchButton');
 
-//clear
-resultsContainer.innerHTML = '';
+  //make h1 disappear after loading the results of the search
+  h1Elem.style.display = 'none';
+  //clear button - search input
+  searchInputElem.style.display = 'none';
+  searchButtonElem.style.display = 'none';
+  //clear containers
+  resultsContainer.innerHTML = '';
   descriptionContainer.innerHTML = '';
-  
+
   fetch(`https://openlibrary.org/subjects/${searchInput}.json`)
     .then(response => {
       if (!response.ok) {
@@ -18,15 +22,18 @@ resultsContainer.innerHTML = '';
       }
       return response.json();
     })
+
     .then(data => {
       if (!data.works || data.works.length === 0) {
-        throw new Error('No books found for the given category. Please check your spelling and try again.');
+        throw new Error('No books found for the given category. Please try again.');
       }
 
       data.works.forEach(work => {
         const bookElement = document.createElement('div');
         bookElement.classList.add('book');
         bookElement.textContent = `Title: ${work.title}, Authors: ${work.authors.map(author => `${author.name}`).join(', ')}`;
+
+
         bookElement.addEventListener('click', () => {
           getBookDescription(work.key);
         });
@@ -40,29 +47,13 @@ resultsContainer.innerHTML = '';
 
 }
 
-//resets the page
-function resetPage() {
-  const searchInputElem = document.getElementById('searchInput');
-  const searchButtonElem = document.getElementById('searchButton');
-  searchInputElem.style.display = 'block';
-  searchButtonElem.style.display = 'block';
-
-  //clear container
-  const descriptionContainer = document.getElementById('descriptionContainer');
-  descriptionContainer.innerHTML = '';
-
-  // Clear results container
-  const resultsContainer = document.getElementById('results');
-  resultsContainer.innerHTML = '';
-}
-
-//gets book description and appends div
+//gets book description -appends div
 
 function getBookDescription(bookKey) {
   const descriptionContainer = document.getElementById('descriptionContainer');
   const h1Elem = document.querySelector('h1');
 
-  // Hide the h1 elem after loading descriptions
+  // Hide the h1 element after loading descriptions
   h1Elem.style.display = 'none';
 
   fetch(`https://openlibrary.org${bookKey}.json`)
@@ -80,10 +71,31 @@ function getBookDescription(bookKey) {
 
       descriptionContainer.innerHTML = '';
       descriptionContainer.appendChild(descriptionDiv);
-      
+
     })
     .catch(error => {
       console.error('Error:', error.message);
       descriptionContainer.innerHTML = `<p>Error: ${error.message}</p>`;
     });
 }
+
+
+//refresh the page
+function resetPage() {
+  const searchInputElem = document.getElementById('searchInput');
+  const searchButtonElem = document.getElementById('searchButton');
+  searchInputElem.style.display = 'block';
+  searchButtonElem.style.display = 'block';
+
+  //clear container
+  const descriptionContainer = document.getElementById('descriptionContainer');
+  descriptionContainer.innerHTML = '';
+
+  // Clear results 
+  const resultsContainer = document.getElementById('results');
+  resultsContainer.innerHTML = ''; const h1Elem = document.querySelector('h1');
+  
+  // Show h1 
+  h1Elem.style.display = 'block';
+}
+
