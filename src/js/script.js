63763a87e547d20a0a47de393d85d6a8ad317c6b
Ale <vale.alessia@yahoo.it>
2023-12-08 11:13:21 +0100

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,100 +1,107 @@
-//  search/fetch books
+// Fetch books
 function fetchBooks(searchInput) {
-  return fetch(`https://openlibrary.org/subjects/${searchInput}.json`)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Category not found. Please check your spelling and try again.');
-          }
-          return response.json();
-      });
+    return fetch(`https://openlibrary.org/subjects/${searchInput}.json`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Category not found. Please check your spelling and try again.');
+            }
+            return response.json();
+        });
 }
 
-// render/fetch one book 
-function renderBook(work, resultsContainer) {
-  const bookElement = document.createElement('div');
-  bookElement.classList.add('book');
-  bookElement.textContent = `Title: ${work.title}, Authors: ${work.authors.map(author => `${author.name}`).join(', ')}`;
-  bookElement.addEventListener('click', () => {
-      getBookDescription(work.key);
-  });
-  resultsContainer.appendChild(bookElement);
+// Render one book
+function renderBook(work) {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+    bookElement.textContent = `Title: ${work.title}, Authors: ${work.authors.map(author => `${author.name}`).join(', ')}`;
+    bookElement.addEventListener('click', () => {
+        getBookDescription(work.key);
+    });
+    return bookElement;
 }
 
-function handleBookError(error, resultsContainer) {
-  console.error('Error:', error.message); // error handling 
-  resultsContainer.innerHTML = `<p>Error: ${error.message}</p>`;
+function handleBookFetchError(error, resultsContainer) {
+    console.error('Error:', error.message);
+    resultsContainer.innerHTML = `<p>Error: ${error.message}</p>`;
 }
 
-// render/fetch book(s)
+// Search books
 function searchBooks() {
-  const searchInput = document.getElementById('searchInput').value.toLowerCase();
-  const resultsContainer = document.getElementById('results');
-  const descriptionContainer = document.getElementById('descriptionContainer');
-  const h1Elem = document.querySelector('h1');
-  const searchInputElem = document.getElementById('searchInput');
-  const searchButtonElem = document.getElementById('searchButton');
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const resultsContainer = document.getElementById('results');
+    const descriptionContainer = document.getElementById('descriptionContainer');
+    const searchInputElem = document.getElementById('searchInput');
+    const searchButtonElem = document.getElementById('searchButton');
+    const h1Elem = document.querySelector('h1');
+    h1Elem.style.display = 'none';
 
-  h1Elem.style.display = 'none';
+    fetchBooks(searchInput)
+        .then(data => {
+            if (!data.works || data.works.length === 0) {
+                throw new Error('No books found for the given category. Please try again.');
+            }
 
-  fetchBooks(searchInput)
-      .then(data => {
-          if (!data.works || data.works.length === 0) {
-              throw new Error('No books found for the given category. Please try again.');
-          }
+            data.works.forEach(work => {
+                const bookElement = renderBook(work);
+                resultsContainer.appendChild(bookElement);
+            });
+        })
+        .catch(error => {
+            handleBookFetchError(error, resultsContainer);
+        });
 
-          data.works.forEach(work => {
-              renderBook(work, resultsContainer);
-          });
-      })
-      .catch(error => {
-          handleBookFetchError(error, resultsContainer);
-      });
-
-  // clear buttons / empty containers 
-  searchInputElem.style.display = 'none';
-  searchButtonElem.style.display = 'none';
-  resultsContainer.innerHTML = '';
-  descriptionContainer.innerHTML = '';
+    // Clear buttons / empty containers 
+    searchInputElem.style.display = 'none';
+    searchButtonElem.style.display = 'none';
+    resultsContainer.innerHTML = '';
+    descriptionContainer.innerHTML = '';
 }
 
-// displaying book description
+// Displaying description
 function getBookDescription(bookKey) {
-  const descriptionContainer = document.getElementById('descriptionContainer');
-  const h1Elem = document.querySelector('h1');
-  h1Elem.style.display = 'none'; //hide h1 after
+    const descriptionContainer = document.getElementById('descriptionContainer');
+    const h1Elem = document.querySelector('h1');
+    h1Elem.style.display = 'none';
 
-  fetch(`https://openlibrary.org${bookKey}.json`)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Book not found. Please check your spelling and try again.');
-          }
-          return response.json();
-      })
-      .then(book => {
-          // and add divs
-          const descriptionDiv = document.createElement('div');
-          descriptionDiv.classList.add('book-description');
-          descriptionDiv.innerHTML = `<p>Synopsis:</p><p>${book.description}</p>`;
-          descriptionContainer.innerHTML = '';
-          descriptionContainer.appendChild(descriptionDiv);
-      })
-      .catch(error => {
-          console.error('Error:', error.message);
-          descriptionContainer.innerHTML = `<p>Error: ${error.message}</p>`;
-      });
+    fetch(`https://openlibrary.org${bookKey}.json`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Book not found. Please check your spelling and try again.');
+            }
+            return response.json();
+        })
+        .then(book => {
+            const descriptionDiv = createDescriptionDiv(book);
+            descriptionContainer.innerHTML = '';
+            descriptionContainer.appendChild(descriptionDiv);
+        })
+        .catch(error => {
+            handleBookFetchError(error, descriptionContainer);
+        });
 }
 
-// reset / refresh
+// Creating description div
+function createDescriptionDiv(book) {
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.classList.add('book-description');
+    descriptionDiv.innerHTML = `<p>Synopsis:</p><p>${book.description}</p>`;
+    return descriptionDiv;
+}
+
+// Reset / Refresh
 function resetPage() {
-  const searchInputElem = document.getElementById('searchInput');
-  const searchButtonElem = document.getElementById('searchButton');
-  const h1Elem = document.querySelector('h1');
-  searchInputElem.style.display = 'block';
-  searchButtonElem.style.display = 'block';
-  // Clear containers / results
-  const descriptionContainer = document.getElementById('descriptionContainer');
-  descriptionContainer.innerHTML = '';
-  const resultsContainer = document.getElementById('results');
-  resultsContainer.innerHTML = '';
-  h1Elem.style.display = 'block';
+    const searchInputElem = document.getElementById('searchInput');
+    const searchButtonElem = document.getElementById('searchButton');
+    const h1Elem = document.querySelector('h1');
+    searchInputElem.style.display = 'block';
+    searchButtonElem.style.display = 'block';
+
+    // Clear containers / results
+    const descriptionContainer = document.getElementById('descriptionContainer');
+    descriptionContainer.innerHTML = '';
+
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = '';
+
+    h1Elem.style.display = 'block';
 }
